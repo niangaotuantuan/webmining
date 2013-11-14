5560292e80a7e3faa7c955b97b1356f80aab2c8b
SolessChong <solesschong@gmail.com>
2013-11-14 15:26:26 +0800

    refactoring

--- python_src/scikit.py
+++ python_src/scikit.py
@@ -1,12 +1,12 @@
 from sklearn.decomposition import TruncatedSVD
 from sklearn.svm import LinearSVC
-from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectKBest, chi2
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-import numpy as np
 from sklearn.svm import SVR
+import numpy as np
+import pandas
 import matplotlib.pyplot as plt
 
 execfile('scratch.py')
@@ -16,24 +16,70 @@ def tokenizer(text):
 	stopwords = nltk.corpus.stopwords.words('english')
 	return [w.lower() for w in tok.tokenize(text) if w.lower() not in stopwords]
 
+###############################################################
+# Routines
+##########
+def read_dataset(input_filename):
+	# Read files
+	tweets = read_tweets(input_filename)
+	labels = read_labels(input_filename)	
 
-# Read files
-input_filename = '../data/train_lite_1000.csv'
-tweets = read_tweets(input_filename)
-labels = read_labels(input_filename)
-# Get feature vectors by tf-idf
-vec = TfidfVectorizer(tokenizer=tokenizer, max_features=50)
-data = vec.fit_transform(tweets).toarray()
+	# Get feature vectors by tf-idf
+	vec = TfidfVectorizer(tokenizer=tokenizer, max_features=50)
+	data = vec.fit_transform(tweets).toarray()
 
-# SVM regression
-svr_rbf = SVR(kernel='rbf', C=1e3)
-label1 = zip(*labels)[0]
-fold = int(len(data) * 0.9)
-model_rbf = svr_rbf.fit(data[0:fold], label1[0:fold])
-pred = model_rbf.predict(data[fold+1:])
+	return data, labels
 
-# Evaluate
-nlabel1 = np.float32(np.array(label1[fold+1:]))
-npred = np.float32(np.array(pred))
+# Train model
+def train_model(train_data, train_labels, algo):
+	if algo == 'svr_rbf':
+		"""
+		SVM regression
+		"""
+		svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+		svr_rbf.fit(train_data, train_labels)
+		return svr_rbf
 
-nlabel1 - npred
+	# No hit algorithm
+	print "unimplemented model type"
+	return None
+
+# Get prediction
+def get_prediction(model, test_data):
+	return model.predict(test_data)
+
+def evaluate_result(pred, test_labels):
+	"""
+	Evaluate the result. 
+	Print stats description and return the error series
+	"""
+	npred = np.float32(np.array(pred))
+	nlabels = np.float32(np.array(test_labels))
+
+	s = Series(np.abs(nlabels - npred))
+	s.describe()
+
+	return s
+
+# Main routine
+def routine():
+	# Prepare data
+	data, labels = read_dataset('../data/train_lite_1000.csv')
+	labels = zip(*labels)[0]
+	# Seperate the test and train
+	fold = int(len(data) * 0.9)
+	train_data = data[0:fold]
+	train_labels = labels[0:fold]
+	test_data = data[fold+1:]
+	test_labels = labels[fold+1:]
+	
+	# Train model
+	model = train_model(train_data, train_labels, 'svr_rbf')
+
+	# Get predict result
+	result = get_prediction(model, test_data)
+
+	# Evaluate result
+	evaluate_result(result, test_labels)
+
+	pass
